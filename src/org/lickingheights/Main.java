b0c08150d48267f9f4c575f8fc9d4b16138af905
Karen <karen.bandong.2018@student.lhschools.org>
2019-01-30 18:11:46 -0500

    Phase 1 and 2 complete including Scanner set up.

---
+++ src/org/lickingheights/Main.java
@@ -0,0 +1,15 @@
+package org.lickingheights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Phase 1 and 2
+        double pounds, inches;
+        String Username, greetings;
+        greetings= "Hello!";
+        Scanner keyboard= new Scanner(System.int);
+        
+    }
+}
